3aa5a8d7138d7bcba8839baab00f7e927377b25b
Behrouz Khezry <bkhezry@gmail.com>
2018-10-08 20:43:04 +0330

    create change language function in settings fragment.

--- app/src/main/java/com/github/bkhezry/learn2learn/ui/fragment/SettingsFragment.java
+++ app/src/main/java/com/github/bkhezry/learn2learn/ui/fragment/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.github.bkhezry.learn2learn.ui.fragment;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +10,26 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.github.bkhezry.learn2learn.LauncherActivity;
 import com.github.bkhezry.learn2learn.R;
+import com.github.bkhezry.learn2learn.util.Constant;
 import com.github.pwittchen.prefser.library.rx2.Prefser;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.DialogFragment;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class SettingsFragment extends DialogFragment {
 
 
+  @BindView(R.id.persian_image_view)
+  AppCompatImageView persianImageView;
+  @BindView(R.id.english_image_view)
+  AppCompatImageView englishImageView;
   private Activity activity;
   private Prefser prefser;
 
@@ -29,6 +39,7 @@ public class SettingsFragment extends DialogFragment {
     ButterKnife.bind(this, rootView);
     activity = getActivity();
     prefser = new Prefser(activity);
+    setUpLocale();
     return rootView;
   }
 
@@ -51,5 +62,44 @@ public class SettingsFragment extends DialogFragment {
     if (getFragmentManager() != null) {
       getFragmentManager().popBackStackImmediate();
     }
+  }
+
+  @OnClick({R.id.persian_image_view, R.id.english_image_view})
+  void handleLanguage(View view) {
+    switch (view.getId()) {
+      case R.id.persian_image_view:
+        prefser.put(Constant.LANGUAGE, "fa");
+        break;
+      case R.id.english_image_view:
+        prefser.put(Constant.LANGUAGE, "en");
+        break;
+    }
+    restartApp();
+  }
+
+  private void setUpLocale() {
+    if (prefser.contains(Constant.LANGUAGE)) {
+      if (prefser.get(Constant.LANGUAGE, String.class, null).equals("fa")) {
+        changeLanguagePersian();
+      } else {
+        changeLanguageEnglish();
+      }
+    }
+  }
+
+  private void restartApp() {
+    Intent i = new Intent(activity, LauncherActivity.class);
+    startActivity(i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+    System.exit(0);
+  }
+
+  private void changeLanguagePersian() {
+    persianImageView.setBackgroundResource(R.drawable.image_border);
+    englishImageView.setBackgroundResource(android.R.color.transparent);
+  }
+
+  private void changeLanguageEnglish() {
+    englishImageView.setBackgroundResource(R.drawable.image_border);
+    persianImageView.setBackgroundResource(android.R.color.transparent);
   }
 }
