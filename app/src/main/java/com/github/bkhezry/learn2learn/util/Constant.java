2b1f8fdf55a7ef5b83a60e4a061b55d493a66a85
Behrouz Khezry <bkhezry@gmail.com>
2018-10-22 22:46:25 +0330

    change application server.

--- app/src/main/java/com/github/bkhezry/learn2learn/util/Constant.java
+++ app/src/main/java/com/github/bkhezry/learn2learn/util/Constant.java
@@ -2,7 +2,7 @@ package com.github.bkhezry.learn2learn.util;
 
 public class Constant {
   public static final String LANGUAGE = "language";
-  public static final String BASE_URL = "http://192.168.1.100/api/v1/";
+  public static final String BASE_URL = "http://api.coordinates.ir/api/v1/";
   public static final String TOKEN = "token";
   public static final String GRAVATAR_URL = "https://www.gravatar.com/avatar/";
   public static final String IS_DARK_THEME = "is-dark-theme";
