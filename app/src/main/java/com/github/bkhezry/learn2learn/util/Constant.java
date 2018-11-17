1e0e925a71ad97cb7de3b34b64aaad2936b53206
Behrouz Khezry <bkhezry@gmail.com>
2018-11-17 11:33:07 +0330

    change server base url.

--- app/src/main/java/com/github/bkhezry/learn2learn/util/Constant.java
+++ app/src/main/java/com/github/bkhezry/learn2learn/util/Constant.java
@@ -2,7 +2,7 @@ package com.github.bkhezry.learn2learn.util;
 
 public class Constant {
   public static final String LANGUAGE = "language";
-  public static final String BASE_URL = "http://api.coordinates.ir/api/v1/";
+  public static final String BASE_URL = "https://api.mahadel.ir/api/v1/";
   public static final String TOKEN = "token";
   public static final String GRAVATAR_URL = "https://www.gravatar.com/avatar/";
   public static final String IS_DARK_THEME = "is-dark-theme";
