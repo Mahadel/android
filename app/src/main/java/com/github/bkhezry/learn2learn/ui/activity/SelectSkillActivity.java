4e3d038aa2dc0f35daec4cd9d5f3e5da4c090f3e
Behrouz Khezry <bkhezry@gmail.com>
2018-11-22 00:49:26 +0330

    separate categories from skills in SelectSkillActivity.

--- app/src/main/java/com/github/bkhezry/learn2learn/ui/activity/SelectSkillActivity.java
+++ app/src/main/java/com/github/bkhezry/learn2learn/ui/activity/SelectSkillActivity.java
@@ -3,31 +3,26 @@ package com.github.bkhezry.learn2learn.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.github.bkhezry.learn2learn.R;
-import com.github.bkhezry.learn2learn.StickyHeaderAdapter;
 import com.github.bkhezry.learn2learn.model.Category;
 import com.github.bkhezry.learn2learn.model.SkillsItem;
 import com.github.bkhezry.learn2learn.model.UserSkill;
 import com.github.bkhezry.learn2learn.util.AppUtil;
 import com.github.bkhezry.learn2learn.util.Constant;
 import com.github.bkhezry.learn2learn.util.DatabaseUtil;
+import com.github.bkhezry.learn2learn.util.GridSpacingItemDecoration;
 import com.github.bkhezry.learn2learn.util.MyApplication;
 import com.mikepenz.fastadapter.FastAdapter;
-import com.mikepenz.fastadapter.IAdapter;
 import com.mikepenz.fastadapter.adapters.ItemAdapter;
-import com.mikepenz.fastadapter.listeners.OnClickListener;
-import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
 
-import java.util.Arrays;
 import java.util.List;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,13 +30,17 @@ import butterknife.OnClick;
 import io.objectbox.Box;
 import io.objectbox.BoxStore;
 
+import static com.github.bkhezry.learn2learn.util.AppUtil.dpToPx;
+
 public class SelectSkillActivity extends BaseActivity {
   @BindView(R.id.recycler_view)
   RecyclerView recyclerView;
   private Box<SkillsItem> skillsItemBox;
   private Box<Category> categoryBox;
   private Box<UserSkill> userSkillBox;
-  private FastAdapter<SkillsItem> fastAdapter;
+  private FastAdapter<SkillsItem> mFastAdapterSkill;
+  private FastAdapter<Category> mFastAdapterCategory;
+  private ItemAdapter<Category> mItemAdapterCategory;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -59,19 +58,22 @@ public class SelectSkillActivity extends BaseActivity {
   }
 
   private void initRecyclerViews() {
-    //create our adapters
-    final StickyHeaderAdapter<SkillsItem> stickyHeaderAdapter = new StickyHeaderAdapter<>();
-    final ItemAdapter headerAdapter = new ItemAdapter();
-    final ItemAdapter<SkillsItem> itemAdapter = new ItemAdapter<>();
-    fastAdapter = FastAdapter.with(Arrays.asList(headerAdapter, itemAdapter));
-    fastAdapter.withSelectable(true);
-    RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+    RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
     recyclerView.setLayoutManager(mLayoutManager);
-    recyclerView.setAdapter(stickyHeaderAdapter.wrap(fastAdapter));
-    //this adds the Sticky Headers within our list
-    final StickyRecyclerHeadersDecoration decoration = new StickyRecyclerHeadersDecoration(stickyHeaderAdapter);
-    recyclerView.addItemDecoration(decoration);
-    List<SkillsItem> skillsItems = skillsItemBox.getAll();
+    recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(6, getResources()), true));
+    mItemAdapterCategory = new ItemAdapter<>();
+    mFastAdapterCategory = FastAdapter.with(mItemAdapterCategory);
+    recyclerView.setAdapter(mFastAdapterCategory);
+    getCategory();
+  }
+
+  private void getCategory() {
+    List<Category> categories = categoryBox.getAll();
+    mItemAdapterCategory.clear();
+    mItemAdapterCategory.add(categories);
+  }
+
+  private void separateLists(List<SkillsItem> skillsItems) {
     if (AppUtil.isRTL(this)) {
       for (SkillsItem skillsItem : skillsItems) {
         Category category = DatabaseUtil.getCategoryWithUUID(categoryBox, skillsItem.getCategoryUuid());
@@ -85,29 +87,17 @@ public class SelectSkillActivity extends BaseActivity {
         skillsItem.withIdentifier(category.getId());
       }
     }
-    itemAdapter.add(skillsItems);
-    //so the headers are aware of changes
-    stickyHeaderAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-      @Override
-      public void onChanged() {
-        decoration.invalidateHeaders();
-      }
-    });
-    fastAdapter.withOnClickListener(new OnClickListener<SkillsItem>() {
-      @Override
-      public boolean onClick(View v, @NonNull IAdapter<SkillsItem> adapter, @NonNull SkillsItem item, int position) {
-        if (!isUserSkillDuplicate(item)) {
-          Intent resultIntent = new Intent();
-          resultIntent.putExtra(Constant.SKILL_ITEM, item);
-          setResult(Activity.RESULT_OK, resultIntent);
-          finish();
-        } else {
-          Toast.makeText(SelectSkillActivity.this, getString(R.string.skill_exists_warning), Toast.LENGTH_SHORT).show();
-        }
-        return true;
-      }
-    });
+  }
 
+  private void returnResult(@NonNull SkillsItem item) {
+    if (!isUserSkillDuplicate(item)) {
+      Intent resultIntent = new Intent();
+      resultIntent.putExtra(Constant.SKILL_ITEM, item);
+      setResult(Activity.RESULT_OK, resultIntent);
+      finish();
+    } else {
+      Toast.makeText(SelectSkillActivity.this, getString(R.string.skill_exists_warning), Toast.LENGTH_SHORT).show();
+    }
   }
 
   private boolean isUserSkillDuplicate(SkillsItem item) {
