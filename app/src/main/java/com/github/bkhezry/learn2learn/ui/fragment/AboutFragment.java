a68f081e72dd859ce557ea57c4da61c82e88e49c
Behrouz Khezry <bkhezry@gmail.com>
2018-11-24 17:01:03 +0330

    handle click event in about fragment items.

--- app/src/main/java/com/github/bkhezry/learn2learn/ui/fragment/AboutFragment.java
+++ app/src/main/java/com/github/bkhezry/learn2learn/ui/fragment/AboutFragment.java
@@ -2,7 +2,10 @@ package com.github.bkhezry.learn2learn.ui.fragment;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.blankj.utilcode.util.SnackbarUtils;
 import com.github.bkhezry.learn2learn.R;
 import com.github.bkhezry.learn2learn.model.About;
 import com.github.bkhezry.learn2learn.model.AuthenticationInfo;
@@ -18,6 +22,7 @@ import com.github.bkhezry.learn2learn.util.AppUtil;
 import com.github.bkhezry.learn2learn.util.Constant;
 import com.github.bkhezry.learn2learn.util.RetrofitUtil;
 import com.github.pwittchen.prefser.library.rx2.Prefser;
+import com.google.android.material.button.MaterialButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatTextView;
@@ -35,10 +40,17 @@ public class AboutFragment extends DialogFragment {
 
   @BindView(R.id.version_app_text_view)
   AppCompatTextView versionAppTextView;
+  @BindView(R.id.update_button)
+  MaterialButton updateButton;
+  @BindView(R.id.sponsor_name)
+  AppCompatTextView sponsorName;
+  @BindView(R.id.sponsor_description_text_view)
+  AppCompatTextView sponsorDescriptionTextView;
   private Prefser prefser;
   private Dialog loadingDialog;
   private Activity activity;
   private About about;
+  private String versionName;
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -69,17 +81,23 @@ public class AboutFragment extends DialogFragment {
 
       @Override
       public void onFailure(@NonNull Call<About> call, @NonNull Throwable t) {
+        t.printStackTrace();
         loadingDialog.dismiss();
       }
     });
   }
 
   private void handleAbout() {
+    if (!about.getAppVersion().equals(versionName)) {
+      updateButton.setVisibility(View.VISIBLE);
+    }
+    sponsorName.setText(about.getSponsorName());
+    sponsorDescriptionTextView.setText(about.getSponsorDescription());
 
   }
 
   private void setAppVersion() {
-    String versionName = "";
+    versionName = "";
     try {
       versionName = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
     } catch (PackageManager.NameNotFoundException e) {
@@ -108,5 +126,37 @@ public class AboutFragment extends DialogFragment {
     if (getFragmentManager() != null) {
       getFragmentManager().popBackStackImmediate();
     }
+  }
+
+  @OnClick({R.id.update_button, R.id.changelog_layout, R.id.license_layout, R.id.sponsor_website, R.id.developer_layout})
+  void handleLayoutClicks(View view) {
+    switch (view.getId()) {
+      case R.id.update_button:
+        startBrowser(about.getAppUrl(), view);
+        break;
+      case R.id.changelog_layout:
+        startBrowser(about.getChangelogUrl(), view);
+        break;
+      case R.id.license_layout:
+        startBrowser(about.getLicenseUrl(), view);
+        break;
+      case R.id.sponsor_website:
+        startBrowser(about.getSponsorUrl(), view);
+        break;
+      case R.id.developer_layout:
+        startBrowser(getString(R.string.bkhezry_twitter_url), view);
+        break;
+    }
+  }
+
+  private void startBrowser(String url, View view) {
+    try {
+      Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+      startActivity(browserIntent);
+    } catch (ActivityNotFoundException e) {
+      AppUtil.showSnackbar(view, "برنامه مرورگر یافت نشد", activity, SnackbarUtils.LENGTH_INDEFINITE);
+      e.printStackTrace();
+    }
+
   }
 }
