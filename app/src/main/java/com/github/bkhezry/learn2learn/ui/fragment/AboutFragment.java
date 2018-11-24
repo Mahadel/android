131665f0a9bef76872b905db39020d6ddacb5906
Behrouz Khezry <bkhezry@gmail.com>
2018-11-24 16:21:27 +0330

    get about from server.

--- app/src/main/java/com/github/bkhezry/learn2learn/ui/fragment/AboutFragment.java
+++ app/src/main/java/com/github/bkhezry/learn2learn/ui/fragment/AboutFragment.java
@@ -1,5 +1,6 @@
 package com.github.bkhezry.learn2learn.ui.fragment;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -10,6 +11,13 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import com.github.bkhezry.learn2learn.R;
+import com.github.bkhezry.learn2learn.model.About;
+import com.github.bkhezry.learn2learn.model.AuthenticationInfo;
+import com.github.bkhezry.learn2learn.service.APIService;
+import com.github.bkhezry.learn2learn.util.AppUtil;
+import com.github.bkhezry.learn2learn.util.Constant;
+import com.github.bkhezry.learn2learn.util.RetrofitUtil;
+import com.github.pwittchen.prefser.library.rx2.Prefser;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatTextView;
@@ -17,6 +25,9 @@ import androidx.fragment.app.DialogFragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class AboutFragment extends DialogFragment {
@@ -24,13 +35,47 @@ public class AboutFragment extends DialogFragment {
 
   @BindView(R.id.version_app_text_view)
   AppCompatTextView versionAppTextView;
+  private Prefser prefser;
+  private Dialog loadingDialog;
+  private Activity activity;
+  private About about;
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_about, container, false);
     ButterKnife.bind(this, rootView);
+    activity = getActivity();
+    prefser = new Prefser(activity);
+    loadingDialog = AppUtil.getDialogLoading(activity);
     setAppVersion();
+    getAbout();
     return rootView;
+  }
+
+  private void getAbout() {
+    loadingDialog.show();
+    AuthenticationInfo info = prefser.get(Constant.TOKEN, AuthenticationInfo.class, null);
+    APIService apiService = RetrofitUtil.getRetrofit(info.getToken()).create(APIService.class);
+    Call<About> call = apiService.getAbout();
+    call.enqueue(new Callback<About>() {
+      @Override
+      public void onResponse(@NonNull Call<About> call, @NonNull Response<About> response) {
+        loadingDialog.dismiss();
+        if (response.isSuccessful()) {
+          about = response.body();
+          handleAbout();
+        }
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<About> call, @NonNull Throwable t) {
+        loadingDialog.dismiss();
+      }
+    });
+  }
+
+  private void handleAbout() {
+
   }
 
   private void setAppVersion() {
