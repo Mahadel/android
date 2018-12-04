b21ecb68a29b7f122671bb9948046ac03d80f00f
Behrouz Khezry <bkhezry@gmail.com>
2018-12-04 21:08:28 +0330

    add NonNull annotation in a util class.

--- app/src/main/java/com/github/mahadel/demo/util/GridSpacingItemDecoration.java
+++ app/src/main/java/com/github/mahadel/demo/util/GridSpacingItemDecoration.java
@@ -3,6 +3,7 @@ package com.github.mahadel.demo.util;
 import android.graphics.Rect;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
@@ -17,7 +18,7 @@ public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
   }
 
   @Override
-  public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+  public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
     int position = parent.getChildAdapterPosition(view); // item position
     int column = position % spanCount; // item column
 
