6faabc0afdfb6d190cb61bd30bdccca6ea9887e2
Behrouz Khezry <bkhezry@gmail.com>
2018-12-09 15:27:59 +0330

    add javadoc to CallbackResult interface.

--- app/src/main/java/com/github/mahadel/demo/listener/CallbackResult.java
+++ app/src/main/java/com/github/mahadel/demo/listener/CallbackResult.java
@@ -3,6 +3,13 @@ package com.github.mahadel.demo.listener;
 import com.github.mahadel.demo.model.UserSkill;
 import com.github.mahadel.demo.util.AppUtil;
 
+/**
+ * send result of UserSkill that add to the user.
+ */
 public interface CallbackResult {
+  /**
+   * @param userSkill {@link UserSkill} added user skill
+   * @param skillType {@link com.github.mahadel.demo.util.AppUtil.SkillType} type of skill that added
+   */
   void sendResult(UserSkill userSkill, AppUtil.SkillType skillType);
 }
