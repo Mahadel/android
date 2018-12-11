6a9f3afb67f4a0e92dd06dce34fab1233a65a2c5
Behrouz Khezry <bkhezry@gmail.com>
2018-12-11 15:35:52 +0330

    add function to the Firebase event log params.

--- app/src/main/java/com/github/mahadel/demo/util/FirebaseEventLog.java
+++ app/src/main/java/com/github/mahadel/demo/util/FirebaseEventLog.java
@@ -4,9 +4,10 @@ import android.os.Bundle;
 
 public class FirebaseEventLog {
 
-  public static void log(String event, String tag, String message) {
+  public static void log(String event, String tag, String function, String message) {
     Bundle bundle = new Bundle();
     bundle.putString("tag", tag);
+    bundle.putString("function", function);
     bundle.putString("error_message", message);
     MyApplication.getFirebaseAnalytics().logEvent(event, bundle);
   }
